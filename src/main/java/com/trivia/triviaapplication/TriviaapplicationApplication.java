0a3c0125ec8a65d79043950890b5e1f038601daa
Vyacheslav-Pan89 <vyacheslavpan89@gmail.com>
2025-10-22 20:19:01 +0300

    initial init

---
+++ src/main/java/com/trivia/triviaapplication/TriviaapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.trivia.triviaapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaapplicationApplication.class, args);
+	}
+
+}
