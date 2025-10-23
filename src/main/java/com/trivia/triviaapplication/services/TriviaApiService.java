37e86bce0e78504ee31f1029f34dc29f89673315
Vyacheslav-Pan89 <vyacheslavpan89@gmail.com>
2025-10-23 21:27:41 +0300

    Added some code and tests for TriviaApiService

--- src/main/java/com/trivia/triviaapplication/services/TriviaApiService.java
+++ src/main/java/com/trivia/triviaapplication/services/TriviaApiService.java
@@ -4,6 +4,7 @@ import com.trivia.triviaapplication.dtos.CategoryResponse;
 import com.trivia.triviaapplication.dtos.Question;
 import com.trivia.triviaapplication.dtos.TriviaResponse;
 import com.trivia.triviaapplication.exceptions.TriviaApiRequestException;
+import com.trivia.triviaapplication.exceptions.TriviaApiResponseException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -38,8 +39,11 @@ public class TriviaApiService {
         try {
             CategoryResponse categoryResponse = restTemplate
                     .getForObject(apiUrl + "/api_category.php", CategoryResponse.class);
-            if (categoryResponse == null || categoryResponse.getTriviaCategories() == null ) {
-                throw new TriviaApiRequestException("No response or invalid response from Trivia API");
+            if (categoryResponse == null || categoryResponse.getTriviaCategories() == null) {
+                throw new TriviaApiResponseException("No response or invalid response from Trivia API");
+            }
+            if(categoryResponse.getTriviaCategories().isEmpty()){
+                throw new TriviaApiResponseException("Trivia API response is empty");
             }
             return categoryResponse;
         } catch (RestClientException e) {
